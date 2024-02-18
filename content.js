f1a03c7bf2912c910fa3a1eb22ab98233f179cd5
Rayzen7 <140942173+Rayzen7@users.noreply.github.com>
2024-02-18 18:45:48 +0700

    Add files via upload

---
+++ content.js
@@ -0,0 +1,30 @@
+// Scrolling navbar
+jQuery(function($) {
+    var $navbar = $('.navbar');
+  
+    updateNavbar();
+  
+    $(window).scroll(function() {
+      updateNavbar();
+    });
+  
+    function updateNavbar() {
+      var $current = $(this).scrollTop();
+      if ($current > 0) {
+        $navbar.addClass('navbar-color').removeClass('original-color');
+      } else {
+        $navbar.removeClass('navbar-color').addClass('original-color');
+      }
+    }
+  });
+
+// Responsive navbar
+document.addEventListener("DOMContentLoaded", function () {
+    const navbar = document.querySelector('.navbar');
+    const listIcon = document.querySelector('.list');
+  
+    listIcon.addEventListener('click', function () {
+        navbar.classList.toggle('active');
+    });
+  });
+
